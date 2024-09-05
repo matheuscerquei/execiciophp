4a9802ad5ca56410cb3fe713e5cab28af7c095fe
matheuscerquei <103291017+matheuscerquei@users.noreply.github.com>
2024-09-05 20:30:50 -0300

    atualizado

--- execicio5.php
+++ execicio5.php
@@ -6,73 +6,68 @@
     <title>execicio5</title>
 </head>
 <body>
-<?php
+    <h1>Tigrinho</h1>
+    <form action="" method="post">
+        <label for="option">Selecione a Opção:</label>
+        <select id="option" name="option">
+            <option value="1">1 - Ver Saldo</option>
+            <option value="2">2 - Depositar</option>
+            <option value="3">3 - Sacar</option>
+            <option value="4">4 - Sair</option>
+        </select>
+        <input type="submit" value="Submit">
+    </form>
 
-$balance = 900;
+    <?php
+    $balance = 900;
 
+    if (isset($_POST['option'])) {
+        $option = $_POST['option'];
 
-echo "<h1>Tigrinho</h1>";
-echo "<form action='' method='post'>";
-echo "<label for='option'>Select an option:</label>";
-echo "<select id='option' name='option'>";
-echo "<option value='1'>1 - Ver Saldo</option>";
-echo "<option value='2'>2 - Depositar</option>";
-echo "<option value='3'>3 - Sacar</option>";
-echo "<option value='4'>4 - Sair</option>";
-echo "</select>";
-echo "<input type='submit' value='Submit'>";
-echo "</form>";
-
-
-if (isset($_POST['option'])) {
-  $option = $_POST['option'];
-
-  switch ($option) {
-    case '1':
-     
-      echo "<p>Seu saldo atual é: R$ $balance</p>";
-      break;
-    case '2':
-      
-      echo "<form action='' method='post'>";
-      echo "<label for='amount'>Digite o valor a ser depositado:</label>";
-      echo "<input type='number' id='amount' name='amount'>";
-      echo "<input type='hidden' name='option' value='2'>";
-      echo "<input type='submit' value='Depositar'>";
-      echo "</form>";
-      if (isset($_POST['amount'])) {
-        $amount = $_POST['amount'];
-        $balance += $amount;
-        echo "<p>Valor depositado com sucesso! Seu novo saldo é: R$ $balance</p>";
-      }
-      break;
-    case '3':
-    
-      echo "<form action='' method='post'>";
-      echo "<label for='amount'>Digite o valor a ser sacado:</label>";
-      echo "<input type='number' id='amount' name='amount'>";
-      echo "<input type='hidden' name='option' value='3'>";
-      echo "<input type='submit' value='Sacar'>";
-      echo "</form>";
-      if (isset($_POST['amount'])) {
-        $amount = $_POST['amount'];
-        if ($amount <= $balance) {
-          $balance -= $amount;
-          echo "<p>Valor sacado com sucesso! Seu novo saldo é: R$ $balance</p>";
-        } else {
-          echo "<p>Saldo insuficiente!</p>";
-        }
-      }
-      break;
-    case '4':
-     
-      echo "<p>Até logo!</p>";
-      exit;
-      break;
-    default:
-      echo "<p>Opção inválida!</p>";
-  }
-}
-?>
+        switch ($option) {
+            case '1': ?>
+                <p>Seu saldo atual é: R$ <?= $balance ?></p>
+                <?php break;
+            case '2': ?>
+                <form action="" method="post">
+                    <label for="amount">Digite o valor a ser depositado:</label>
+                    <input type="number" id="amount" name="amount">
+                    <input type="hidden" name="option" value="2">
+                    <input type="submit" value="Depositar">
+                </form>
+                <?php
+                if (isset($_POST['amount'])) {
+                    $amount = $_POST['amount'];
+                    $balance += $amount; ?>
+                    <p>Valor depositado com sucesso! Seu novo saldo é: R$ <?= $balance ?></p>
+                <?php }
+                break;
+            case '3': ?>
+                <form action="" method="post">
+                    <label for="amount">Digite o valor a ser sacado:</label>
+                    <input type="number" id="amount" name="amount">
+                    <input type="hidden" name="option" value="3">
+                    <input type="submit" value="Sacar">
+                </form>
+                <?php
+                if (isset($_POST['amount'])) {
+                    $amount = $_POST['amount'];
+                    if ($amount <= $balance) {
+                        $balance -= $amount; ?>
+                        <p>Valor sacado com sucesso! Seu novo saldo é: R$ <?= $balance ?></p>
+                    <?php } else { ?>
+                        <p>Saldo insuficiente!</p>
+                    <?php }
+                }
+                break;
+            case '4': ?>
+                <p>Até logo!</p>
+                <?php exit;
+                break;
+            default: ?>
+                <p>Opção inválida!</p>
+        <?php }
+    }
+    ?>
 </body>
-</html>
+</ht
